2e52c25f407402a3ad647785ff4ab90fd45d8571
Reajul Hasan Raju <rhraju.dev@gmail.com>
2023-12-03 16:46:47 +0600

    Laravel Migrations

---
+++ laravel/database/migrations/2023_12_02_214941_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up() : void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name')->index();
+            $table->string('last_name')->index();
+            $table->string('phone_number')->unique();
+            $table->string('country')->default("BD");
+            $table->integer('age')->default(18);
+            $table->date('birthdate');
+            $table->string('zip_code');
+            $table->timestamps();
+
+            $table->foreignId("user_id")->constrained("users", "id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down() : void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
