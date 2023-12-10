8db91dcf6508c745b528e301a259ab166f7724ce
Reajul Hasan Raju <rhraju.dev@gmail.com>
2023-12-10 20:55:16 +0600

    PHP is Dead!

---
+++ laravel-migration-extra/database/factories/ProfileFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Profile;
+use Faker\Factory as Faker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Profile>
+ */
+class ProfileFactory extends Factory
+{
+    public function definition() : array
+    {
+        $faker = Faker::create();
+        return [
+            "first_name" => $faker->firstName,
+            "last_name" => $faker->lastName,
+            "mobile" => $faker->phoneNumber,
+            "city" => $faker->city,
+            "shipping_address"=> $faker->streetAddress,
+            // "email"=> $faker->unique()->safeEmail,
+        ];
+    }
+}
---
+++ laravel-migration-extra/database/seeders/ProfileSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Profile;
+
+class ProfileSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        Profile::factory()->count(20)->create();
+    }
+}
