74994c32bc07776d37107173402c0708634ddb3a
Reajul Hasan Raju <rhraju.dev@gmail.com>
2023-12-13 01:36:35 +0600

    DB SEED

---
+++ laravel-migration-extra/database/factories/UserFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as Faker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $faker = Faker::create();
+        return [
+            "email"=> $faker->email,
+            "otp" => $faker->numberBetween(100000,999999),
+            "created_at" => $faker->dateTime(),
+            "updated_at"=> $faker->dateTime()
+        ];
+    }
+}
